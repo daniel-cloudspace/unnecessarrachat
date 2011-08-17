97e4ab7fc2193ce7f67d9239ce617a7a3299b3e1
Daniel <daniel@cloudspace.com>
2011-08-17 19:05:21 -0400

    socket.io installed and working

--- app.js
+++ app.js
@@ -3,36 +3,52 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express')
+var socketio = require('socket.io')
+var eyes = require('eyes')
 
-var app = module.exports = express.createServer();
 
-// Configuration
+
+var app = module.exports = express.createServer()
+  , io = require('socket.io').listen(app)
+
+// Express Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+  app.use(app.router)
+  app.use(express.static(__dirname + '/public'))
+})
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+})
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+  app.use(express.errorHandler())
+})
 
-// Routes
+// Express Routes
 
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Express'
+  })
+})
+
+
+
+// Socket.io Configuration
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+app.listen(8082)
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
